0bcbbf5bd0e71d753d017d19811a7a86b560e0d4
Irfan Backer <irfuvk@gmail.com>
2020-02-16 19:08:17 +0530

    Login + register basic

--- server.js
+++ server.js
@@ -1,15 +1,82 @@
-var express = require('express');
-var passport = require('passport');
+const express = require('express');
+const socket=require("socket.io");
+const passport = require('passport');
 var Strategy = require('passport-local').Strategy;
+
+//-------------------------------------------------MongoDB----------------------------------------------------------------------
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/bookportal',{ useNewUrlParser: true , useUnifiedTopology: true});
+
+var db = mongoose.connection;
+
+db.on('error',function(){
+   console.log('\nMongoDB Connection Error. Please make sure that MongoDB is running\n');
+   process.exit();
+});
+
+var users = mongoose.Schema({username: String,password: String});
+var usersinfo = mongoose.Schema({username: String, firstName: String, lastName: String, email: String, phone: Number});
+
+var availBooks = mongoose.Schema({isbn: Number, seller: String, Name: String,author: String, price: Number});
+var soldBooks = mongoose.Schema({isbn: Number, seller: String, buyer: String, Name: String,author: String, price: Number});
+var reqBooks = mongoose.Schema({isbn: Number, seller: String, Name: String,author: String, price: Number});
+
+// compile schema to model
+var user = mongoose.model('user', users);
+var userinfo = mongoose.model('userinfo', usersinfo);
+var availbook = mongoose.model('availBook', availBooks);
+var soldBook = mongoose.model('soldBook', soldBooks);
+var reqBook = mongoose.model('reqBook', reqBooks);
+
+db.once('open', function() {
+    console.log("Database connection Successful!");
+});
+
+function newUser(nuser){
+  var user1 = new user({ username: nuser.username, password: nuser.password});
+  var info1 = new userinfo({ username: nuser.username, firstName: nuser.firstName, lastName: nuser.lastName, email: nuser.email,phone: nuser.phone});
+  // save model to database
+  user1.save(function (err, user) {
+    if (err) return console.error(err);
+    console.log(nuser.username + " has registered");
+  });
+  info1.save(function (err, userinfo) {
+    if (err) return console.error(err);
+  });
+};
+
+function formValidation(data){
+  var errlist={status:1, error:['ERR1','ERR2']};
+  user.findOne({username: data.username}, function (err, euser) {
+    //console.log(euser+'   ----   '+data.username);
+    if (err) return console.error(err);
+    if(euser.length!=0){
+      console.log(data.username+' repeated');
+      errlist.status=0;
+      errlist.error.push('Username already exists!');
+    }
+  });
+  return errlist;
+};
 
 //-------------------------------------------------PASSPORT----------------------------------------------------------------------
 
 passport.use(new Strategy(
   function(username, password, cb) {
       console.log('checking '+username);
-      if (username!='test') { return cb(null, false); }
-      if (password != 'test') { return cb(null, false); }
-      return cb(null, {username: username});
+      user.findOne({username: username}, function (err, luser) {
+        if (err) return console.error(err);
+        if(luser){
+          if(luser.password==password) return cb(null, {username: username});
+        }
+        return cb(null, false);
+      });
+      //if (username!='test') { return cb(null, false); }
+      //if (password != 'test') { return cb(null, false); }
+      //return cb(null, {username: username});
   }));
 
 passport.serializeUser(function(user, cb) {
@@ -30,8 +97,10 @@ function isLoggedIn(req ,res, next){
 
 //-------------------------------------------------EXPRESS----------------------------------------------------------------------
 
-var app = express();
+const app = express();
+//const { check, validationResult } = require('express-validator');
 
+//app.use(express.json());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(express.static('public'));
@@ -89,6 +158,25 @@ app.use(function (req, res, next) {
 
 //---------------------------------------------------------------------------------------------------------------------
 
-app.listen(3000,function() {
+const listener=app.listen(3000,function() {
   console.log("Your app is listening on port " + 3000);
+});
+
+const io = socket(listener);
+
+//----------------------------SOCKET-----------------------------------------------------------------------------------------
+
+io.on("connection", function(socket) {
+
+  socket.on('checkinput', function(data){
+    var errlist=formValidation(data);
+    socket.emit("inputresponse", errlist);
+    if(errlist.status)  newUser(data);
+    console.log(errlist);
+  });
+
+  socket.on('disconnect', function () {
+      socket.emit('disconnected');
+  });
+
 });
